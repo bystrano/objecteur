8a149d8951fb1c7b1469677d8f97314c92b0a7f3
Michel Bystranowski <bystrano@gmx.ch>
2015-02-07 21:16:49 +0100

    on commence à réécrire objecteur_effacer avec le nouveau système de références

--- inc/objecteur.php
+++ inc/objecteur.php
@@ -149,15 +149,11 @@ function inc_objecteur_effacer_dist ($objets) {
 
             return "définition de l'objet invalide : $err";
         }
+    }
 
-        /* On commence par supprimer les enfants */
-        if (isset($objet['enfants']) AND $enfants = $objet['enfants']) {
+    $liste_objets = objecteur_effacer_calculer_liste($objets);
 
-            $objecteur_effacer = charger_fonction('objecteur_effacer', 'inc');
-            if ($err = $objecteur_effacer($enfants)) {
-                return $err;
-            }
-        }
+    foreach ($liste_objets as $objet) {
 
         $id_objet = objecteur_trouver($objet);
 
@@ -299,15 +295,66 @@ function objecteur_calculer_liste ($objets) {
                 $enfants[$i]['options']['id_parent'] = "@" . $objet['options']['nom'] . "@";
             }
 
-            $ids_enfants = objecteur_calculer_liste($enfants);
+            $liste_enfants = objecteur_calculer_liste($enfants);
 
             unset($objet['enfants']);
             $liste_objets[] = $objet;
-            $liste_objets = array_merge($liste_objets, $ids_enfants);
+            $liste_objets = array_merge($liste_objets, $liste_enfants);
 
         } else {
             $liste_objets[] = $objet;
         }
+    }
+
+    return $liste_objets;
+}
+
+/**
+ * Applatit une arborescence d'objets
+ *
+ * Presque la même chose que objecteur_calculer_liste, mais la
+ * récursion se fait dans l'autre sens, les enfants sont traités en
+ * premier
+ *
+ * @param array $objets : une liste de définitions d'objets
+ * @return array : une liste d'objets sans enfants pour suppression
+ */
+function objecteur_effacer_calculer_liste ($objets) {
+
+    $liste_objets = array();
+
+    foreach ($objets as $objet) {
+
+        /* On commence par supprimer les enfants */
+        if (isset($objet['enfants']) AND $enfants = $objet['enfants']) {
+
+            /* à l'image du paramètre $objets de la fonction
+               objecteur, la clé enfants peut être une définition
+               d'objet plutôt qu'une liste de définitions. */
+            if (isset($enfants['objet'])) {
+                $enfants = array($enfants);
+            }
+
+            /* si l'objet parent n'a pas de nom, on lui en donne un */
+            if ( ! isset($objet['options']['nom'])) {
+
+                $objet['options']['nom'] = '__' . $objet['objet'] . '-'
+                    . $GLOBALS['objecteur_compteur'];
+
+                $GLOBALS['objecteur_compteur'] += 1;
+            }
+
+            foreach ($enfants as $i => $enfant) {
+                $enfants[$i]['options']['id_parent'] = "@" . $objet['options']['nom'] . "@";
+            }
+
+            $liste_enfants = objecteur_effacer_calculer_liste($enfants);
+
+            $liste_objets = array_merge($liste_objets, $liste_enfants);
+            unset($objet['enfants']);
+        }
+
+        $liste_objets[] = $objet;
     }
 
     return $liste_objets;
---
+++ tests/unit/objecteur/objecteur_effacer_calculer_liste.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * Test unitaire de la fonction objecteur_effacer_calculer_liste
+ * du fichier ../plugins/objecteur/inc/objecteur.php
+ *
+ * genere automatiquement par TestBuilder
+ * le 2015-02-07 15:24
+ */
+
+header('Content-Type: text/html; charset=UTF-8');
+
+$test = 'objecteur_effacer_calculer_liste';
+$remonte = "../";
+while (!is_dir($remonte."ecrire"))
+    $remonte = "../$remonte";
+require $remonte.'tests/test.inc';
+find_in_path("../plugins/objecteur/inc/objecteur.php",'',true);
+
+// chercher la fonction si elle n'existe pas
+if (!function_exists($f='objecteur_effacer_calculer_liste')){
+    find_in_path("inc/filtres.php",'',true);
+    $f = chercher_filtre($f);
+}
+
+//
+// hop ! on y va
+//
+$err = tester_fun($f, essais_objecteur_effacer_calculer_liste());
+
+// si le tableau $err est pas vide ca va pas
+if ($err) {
+    die ('<dl>' . join('', $err) . '</dl>');
+}
+
+echo "OK";
+
+
+function essais_objecteur_effacer_calculer_liste(){
+
+    $essais = array (
+        "Suppression d'objet simple" => array (
+            array (
+                array('objet' => 'rubrique'),
+            ),
+            array (
+                array('objet' => 'rubrique'),
+            ),
+        ),
+        "Suppression de plusieurs objets" => array (
+            array (
+                array('objet' => 'rubrique'),
+                array('objet' => 'article'),
+            ),
+            array (
+                array('objet' => 'rubrique'),
+                array('objet' => 'article'),
+            ),
+        ),
+        "Suppression de familles" => array (
+            array (
+                array (
+                    'objet' => 'article',
+                    'options' =>
+                    array (
+                        'id_parent' => '@__rubrique-0@',
+                    ),
+                ),
+                array (
+                    'objet' => 'rubrique',
+                    'options' =>
+                    array (
+                        'nom' => '__rubrique-0',
+                    ),
+                ),
+            ),
+            array (
+                array(
+                    'objet' => 'rubrique',
+                    'enfants' => array(
+                        array(
+                            'objet' => 'article'
+                        ),
+                    ),
+                ),
+            ),
+        ),
+        "Suppression sur trois générations" => array (
+            array (
+                array (
+                    'objet' => 'article',
+                    'options' =>
+                    array (
+                        'id_parent' => '@__rubrique-1@',
+                    ),
+                ),
+                array (
+                    'objet' => 'article',
+                    'options' =>
+                    array (
+                        'id_parent' => '@sousrub@',
+                    ),
+                ),
+                array (
+                    'objet' => 'rubrique',
+                    'options' =>
+                    array (
+                        'nom' => 'sousrub',
+                        'id_parent' => '@__rubrique-1@',
+                    ),
+                ),
+                array (
+                    'objet' => 'rubrique',
+                    'options' =>
+                    array (
+                        'nom' => '__rubrique-1',
+                    ),
+                ),
+            ),
+
+            array (
+                array(
+                    'objet' => 'rubrique',
+                    'enfants' => array(
+                        array(
+                            'objet' => 'article'
+                        ),
+                        array(
+                            'objet' => 'rubrique',
+                            'options' => array(
+                                'nom' => 'sousrub',
+                            ),
+                            'enfants' => array(
+                                array('objet' => 'article'),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    );
+
+    return $essais;
+}
